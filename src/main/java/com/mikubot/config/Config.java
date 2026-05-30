2882c48ab964fdee50bc9189d699c245bf90562f
Sk8rfu <oldernetwork@hotmail.com>
2026-05-30 12:08:04 +0300

    Add files via upload

---
+++ src/main/java/com/mikubot/config/Config.java
@@ -0,0 +1,13 @@
+package com.mikubot.config;
+
+public class Config {
+
+    public static final String TOKEN = "YOUR_BOT_TOKEN";
+
+    public static final long LOG_BAN = 0L;
+    public static final long LOG_KICK = 0L;
+    public static final long LOG_WARN = 0L;
+    public static final long LOG_MUTE = 0L;
+
+    public static final long MUTE_ROLE = 0L;
+}
